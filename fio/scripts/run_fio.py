237e17a903fffcbe11d1d2c2466ed9f598f30a51
Martin Feller <mfel395@sc412436.lan>
2022-11-28 22:42:54 +1300

    include db code

--- fio/scripts/run_fio.py
+++ fio/scripts/run_fio.py
@@ -3,74 +3,73 @@ import sys
 import json
 import subprocess
 import platform
-from datetime import datetime
+import tempfile
+import configparser
+import pymysql.cursors
 
 pltfrm = platform.system().lower()
+if pltfrm != 'windows' and pltfrm != 'linux' and pltfrm != 'darwin':
+    print(f'Unsupported platform: {pltfrm}')
+    sys.exit(1)
 
-if pltfrm == 'windows':
-  basedir = 'c:/windows/temp'
-elif pltfrm == 'linux' or pltfrm == 'darwin':
-  basedir = '/tmp'
-else:
-  print(f'Unsupported platform: {pltfrm}')
-  sys.exit(1)
- 
+config = configparser.ConfigParser()
+config.read('config.ini')
+facility = config['main']['facility']
+
+basedir = tempfile.gettempdir()
 fio_testfile = os.path.join(basedir, 'fio_raw_data')
 fio_output_file = os.path.join(basedir, 'out.json')
 fio_job_file = os.path.join('jobs', f'{pltfrm}.fio')
-facility = 'nectar'
+
+db_conn = pymysql.connect(host=config['mysql']['host'],
+                          port=int(config['mysql']['port']),
+                          user=config['mysql']['user'],
+                          password=config['mysql']['password'],
+                          database=config['mysql']['database'],
+                          cursorclass=pymysql.cursors.DictCursor)
+
 
 def delete_fio_files():
-  if os.path.exists(fio_testfile):
-    os.remove(fio_testfile)
-  if os.path.exists(fio_output_file):
-    os.remove(fio_output_file)
+    for name in [fio_testfile, fio_output_file]:
+        if os.path.exists(name):
+            os.remove(name)
+
 
 def run_fio():
-  cmd_array = ["fio", "--output-format", "json", '--output', fio_output_file, '--filename', fio_testfile, fio_job_file] 
-  proc = subprocess.run(
-    cmd_array,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    text=True
-  )
+    cmd_array = ["fio", "--output-format", "json", '--output', fio_output_file, '--filename', fio_testfile,
+                 fio_job_file]
+    proc = subprocess.run(
+        cmd_array,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
 
-  if proc.returncode != 0:
-    print(proc.stderr)
-    sys.exit(1)
+    if proc.returncode != 0:
+        raise Exception(proc.stderr)
 
-  return proc.returncode
+    return proc.returncode
 
 
-def print_fio_results():
-  with open(fio_output_file, 'r') as f:
-    data = json.load(f)
-  fio_version = data['fio version']
-  timestamp = data['timestamp']
-  date_time = datetime.fromtimestamp(timestamp)
-  global_options = data['global options']
-  ioengine = data['global options']['ioengine']
-  del global_options['ioengine']
-  del global_options['filename']
+def store_fio_results():
+    with open(fio_output_file, 'r') as f:
+        data = json.load(f)
+    timestamp = data['timestamp']
+    global_options = data['global options']
 
-  for job in data['jobs']:
-    job_type = job['job options']['rw']
-    bs = job['job options']['bs']
-    iodepth = job['job options']['iodepth']
-    del job['job options']['rw']
-    del job['job options']['bs']
-    del job['job options']['iodepth']
-    job_options = {**global_options, **job['job options']}
-    print(f'{date_time} | {facility} | {pltfrm} | {job_type} | {ioengine} | {bs} | {iodepth} | {job_options}')
-    for x in ['read', 'write']:
-      if job[x]['io_bytes'] > 0:
-        iops = job[x]['iops'] 
-        bw_mbps = job[x]['bw']
-        print(x + ' BW: ' + str(bw_mbps) + ' KB/s')
-        print(x + ' IOPS: ' + str(iops))
-    print()
+    with db_conn.cursor() as cursor:
+        for job in data['jobs']:
+            job_options = {**global_options, **job['job options']}
+            sql = "INSERT INTO `fio_benchmarks` (`timestamp`, `facility`, 'platform', " \
+                  "'config', 'bw_read_kbs', 'iops_read', 'bw_write_kbs', 'iops_write') " \
+                  "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (timestamp, facility, pltfrm, job_options, job['read']['bw'],
+                                 job['read']['iops'], job['write']['bw'], job['write']['iops']))
+
 
 delete_fio_files()
-run_fio()
-print_fio_results()
-delete_fio_files()
+try:
+    run_fio()
+    store_fio_results()
+finally:
+    delete_fio_files()
